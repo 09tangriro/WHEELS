d455de312c69160aac0daff82b694126e7475341
09tangriro <rkt16@ic.ac.uk>
2019-05-29 19:14:07 +0100

    periodic data

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DeviceControlActivity.java
@@ -0,0 +1,211 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.bluetoothlegatt;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * For a given BLE device, this Activity provides the user interface to connect, display data,
+ * and display GATT services and characteristics supported by the device.  The Activity
+ * communicates with {@code BluetoothLeService}, which in turn interacts with the
+ * Bluetooth LE API.
+ */
+public class DeviceControlActivity extends Activity implements JoystickView.JoystickListener{
+    private final static String TAG = DeviceControlActivity.class.getSimpleName();
+
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private TextView mDataField;
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private BluetoothLeService mBluetoothLeService;
+    private boolean mConnected = false;
+    private int[] data = new int [2];
+
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                invalidateOptionsMenu();
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                invalidateOptionsMenu();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        data[0] = 0;
+        data[1] = 0;
+        JoystickView jView = new JoystickView(this);
+        super.onCreate(savedInstanceState);
+        setContentView(jView);
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                String string_x = Integer.toString(data[0]);
+                String string_y = Integer.toString(data[1]);
+                String message = string_x + "," + string_y;
+                mBluetoothLeService.writeCustomCharacteristic(message);
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 1000,100);
+
+        final Intent intent = getIntent();
+        mDeviceName = intent.getStringExtra(EXTRAS_DEVICE_NAME);
+        mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+
+        // Sets up UI references.
+        /*
+        ((TextView) findViewById(R.id.device_address)).setText(mDeviceAddress);
+        mGattServicesList = (ExpandableListView) findViewById(R.id.gatt_services_list);
+        mGattServicesList.setOnChildClickListener(servicesListClickListner);
+        mConnectionState = (TextView) findViewById(R.id.connection_state);
+        */
+        mDataField = (TextView) findViewById(R.id.data_value);
+
+        getActionBar().setTitle("WHEELS");
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+    }
+    @Override
+    public void onJoystickMoved(float x, float y) {
+        //Log.d("Main Method", "x: " + x + " y: " + y);
+        int integer_x = Math.round(x);
+        int integer_y = Math.round(y);
+        data[0] = integer_x;
+        data[1] = integer_y;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gatt_services, menu);
+        if (mConnected) {
+            menu.findItem(R.id.menu_connect).setVisible(false);
+            menu.findItem(R.id.menu_disconnect).setVisible(true);
+        } else {
+            menu.findItem(R.id.menu_connect).setVisible(true);
+            menu.findItem(R.id.menu_disconnect).setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_connect:
+                mBluetoothLeService.connect(mDeviceAddress);
+                return true;
+            case R.id.menu_disconnect:
+                mBluetoothLeService.disconnect();
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
+    }
+}
---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/JoystickView.java
@@ -0,0 +1,116 @@
+package com.example.android.bluetoothlegatt;
+
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class JoystickView extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+    private float centerX;
+    private float centerY;
+    private float baseRadius;
+    private JoystickListener joystickCallback;
+    private float hatRadius;
+
+    private void setupDimensions() {
+        centerX = getWidth() / 2;
+        centerY = getHeight() / 2;
+        baseRadius = Math.min(getWidth(), getHeight()) / 3;
+        hatRadius = Math.min(getWidth(), getHeight()) / 5;
+    }
+
+    private void drawJoystick(float x, float y) {
+        if (getHolder().getSurface().isValid()) {
+            Canvas newCanvas = this.getHolder().lockCanvas();
+            Paint colours = new Paint();
+            newCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+            colours.setARGB(255, 50, 50, 50);
+            newCanvas.drawCircle(centerX, centerY, baseRadius, colours);
+
+            colours.setARGB(255, 255, 128, 0);
+            newCanvas.drawCircle(x, y, hatRadius, colours);
+
+            getHolder().unlockCanvasAndPost(newCanvas);
+        }
+    }
+
+    public JoystickView(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if (context instanceof JoystickListener) {
+            joystickCallback = (JoystickListener) context;
+        }
+    }
+
+    public JoystickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if (context instanceof JoystickListener) {
+            joystickCallback = (JoystickListener) context;
+        }
+    }
+
+    public JoystickView(Context context, AttributeSet attrs, int style) {
+        super(context, attrs, style);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        if (context instanceof JoystickListener) {
+            joystickCallback = (JoystickListener) context;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        setupDimensions();
+        drawJoystick(centerX, centerY);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    public boolean onTouch(View v, MotionEvent e) {
+        float scalar = 3.57f;
+        if (v.equals(this)) {
+            if (e.getAction() != e.ACTION_UP) {
+                float displacement = (float) Math.sqrt(Math.pow(e.getX() - centerX, 2) + Math.pow(e.getY() - centerY, 2));
+                if (displacement < baseRadius) {
+                    drawJoystick(e.getX(), e.getY());
+                    joystickCallback.onJoystickMoved((e.getX() - centerX) / scalar, (centerY - e.getY()) / scalar);
+                } else {
+                    float ratio = baseRadius / displacement;
+                    float constrainedX = centerX + (e.getX() - centerX) * ratio;
+                    float constrainedY = centerY + (e.getY() - centerY) * ratio;
+                    drawJoystick(constrainedX, constrainedY);
+                    joystickCallback.onJoystickMoved((constrainedX - centerX) / scalar, (centerY - constrainedY) / scalar);
+                }
+            } else {
+                drawJoystick(centerX, centerY);
+                joystickCallback.onJoystickMoved(0, 0);
+            }
+        }
+        return true;
+    }
+
+    public interface JoystickListener {
+        void onJoystickMoved(float x, float y);
+    }
+
+}
