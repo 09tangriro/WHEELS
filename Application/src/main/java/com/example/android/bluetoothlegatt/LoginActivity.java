5a285a74a8c60fb8cda9b86a6e392ca923ac6bd8
09tangriro <rkt16@ic.ac.uk>
2019-06-11 13:11:09 +0100

    password entry

--- Application/src/main/java/com/example/android/bluetoothlegatt/LoginActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/LoginActivity.java
@@ -162,8 +162,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
         }
@@ -195,11 +195,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
         return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
     }
 
     /**
